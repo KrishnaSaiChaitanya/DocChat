26b03220827e2f773bc12c23bc6976c93f1dac38
chaitanya <chaitanya.tata215@gmail.com>
2023-03-06 23:32:01 +0530

    created Main components

---
+++ src/components/Error404.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Error404 = () => {
+  return (
+    <div
+      style={{
+        height: "100vh",
+      }}
+    >
+      <div
+        className=""
+        style={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <img src="./images/error.jpg" style={{ height: "80vh" }} />
+      </div>
+    </div>
+  );
+};
+
+export default Error404;
---
+++ src/components/Home.js
@@ -0,0 +1,53 @@
+import { Button } from "primereact/button";
+import React from "react";
+import { io } from "socket.io-client";
+import Login from "./Signin";
+const Home = () => {
+  // const socket = io("http://localhost:3001");
+  // socket.on("connect", () => {
+  //   console.log("Connected to WebSocket server");
+  // });
+  return (
+    <div>
+      <div
+        className="homeroom_img"
+        style={{
+          height: "70vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <img
+          src="./images/room.jpg"
+          style={{ height: "70vh", padding: "30px" }}
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Button
+          label="Create Room"
+          icon="pi pi-bolt"
+          className="font-bold px-5 py-3 p-button-raised p-button-rounded white-space-nowrap "
+        />
+      </div>
+      <h2 className="text-center pt-3 pb-3">or</h2>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Button label="Join a room" size="lg" text />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
